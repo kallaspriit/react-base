881accfc9ffc65b823b72fe10c54a262fc2ae25d
Priit Kallas <kallaspriit@gmail.com>
2017-04-21 16:59:20 +0300

    Added exception handling examples.

--- server/resolver.js
+++ server/resolver.js
@@ -11,12 +11,15 @@ let counter = 1;
       id
       name
     }
-    asyncUser(id: 2) {
-      id
-      name
-    }
+    #asyncUser(id: 2) {
+    #  id
+    #  name
+    #}
+    #errorMaybe
+    #errorPromise
   }
 }
+
 */
 
 export default {
@@ -42,30 +45,42 @@ export default {
 			return users.find(user => user.id === id);
 		}),
 
-		asyncUser: (({ id }) => {
-			const users = [{
-				id: 1,
-				name: 'Hugh Jass',
-			}, {
-				id: 2,
-				name: 'Jack Mehoff',
-			}, {
-				id: 3,
-				name: 'Al Coholic',
-			}];
+		asyncUser: ({ id }) => new Promise((resolve, _reject) => {
+			setTimeout(() => {
+				const users = [{
+					id: 1,
+					name: 'Hugh Jass',
+				}, {
+					id: 2,
+					name: 'Jack Mehoff',
+				}, {
+					id: 3,
+					name: 'Al Coholic',
+				}];
 
-			const result = users.find(user => user.id === id);
+				const result = users.find(user => user.id === id);
 
-			// fake delay and return promise
-			return new Promise((resolve, _reject) => {
-				setTimeout(() => {
-					resolve(result);
-				}, 1000);
-			});
+				resolve(result);
+			}, 1000);
 		}),
 
 		errorMaybe: () => new Error('maybe test error message'),
 
-		errorPromise: () => new Error('promise test error message'),
+		errorDefinitely: () => new Error('definitely test error message'),
+
+		exceptionMaybe: () => {
+			throw new Error('maybe exception message');
+		},
+
+		exceptionDefinitely: () => {
+			throw new Error('definitely exception message');
+		},
+
+		// TODO this crashes the server, any good way to handle?
+		exceptionPromise: () => new Promise((_resolve, _reject) => {
+			setTimeout(() => {
+				throw new Error('promise exception message');
+			}, 1000);
+		}),
 	},
 };
