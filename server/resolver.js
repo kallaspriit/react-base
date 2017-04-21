830f2524f6c7f44b224f7e13bcec8865c01e5d7a
Priit Kallas <kallaspriit@gmail.com>
2017-04-21 16:26:01 +0300

    Added GraphQL list and object examples.

--- server/resolver.js
+++ server/resolver.js
@@ -3,10 +3,15 @@ let counter = 1;
 
 /*
 {
-	sandbox {
-		message
-		sum(a: 3, b: 5)
-	}
+  sandbox {
+    message
+    sum(a: 3, b: 7)
+    list
+    user(id: 1) {
+      id
+      name
+    }
+  }
 }
 */
 
@@ -15,5 +20,19 @@ export default {
 		message: () => `Hello GraphQL #${counter++}`,
 		sum: ({ a, b }) => a + b,
 		list: () => [1, 2, 3, 4],
+		user: (({ id }) => {
+			const users = [{
+				id: 1,
+				name: 'Hugh Jass',
+			}, {
+				id: 2,
+				name: 'Jack Mehoff',
+			}, {
+				id: 3,
+				name: 'Al Coholic',
+			}];
+
+			return users.find(user => user.id === id);
+		}),
 	},
 };
